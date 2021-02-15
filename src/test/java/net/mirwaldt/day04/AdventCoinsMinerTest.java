f991a0f9587589e9fcdcc68a6e6d3f6fd85305c7
mmirwaldt <michael.mirwaldt@gmail.com>
2021-02-15 22:47:10 +0100

    organized imports

--- src/test/java/net/mirwaldt/day04/AdventCoinsMinerTest.java
+++ src/test/java/net/mirwaldt/day04/AdventCoinsMinerTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
-import java.io.UnsupportedEncodingException;
 import java.security.NoSuchAlgorithmException;
 import java.util.stream.Stream;
 
